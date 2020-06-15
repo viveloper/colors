71314edefc0b016a163aa28d936e9b7331d3ef16
viveloper <usik1234@gmail.com>
2020-06-15 18:54:27 +0900

    implementing sliders

--- app.js
+++ app.js
@@ -3,14 +3,31 @@ const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexs = document.querySelectorAll('.color h2');
 
+let initialColors;
+
+// Add event listener
+sliders.forEach((slider) => {
+  slider.addEventListener('input', hslControls);
+});
+colorDivs.forEach((div, index) => {
+  div.addEventListener('change', () => {
+    updateTextUI(index);
+  });
+});
+
 function generateHex() {
   const hexColor = chroma.random().toString().toUpperCase();
   return hexColor;
 }
 
 function randomColors() {
+  initialColors = [];
+
   colorDivs.forEach((colorDiv, index) => {
     const randomColor = generateHex();
+
+    initialColors.push(chroma(randomColor).hex().toUpperCase());
+
     const hexText = colorDiv.children[0];
 
     // Add the color to background
@@ -52,6 +69,38 @@ function colorizeSliders(color, hue, brightness, saturation) {
     0
   )},${scaleBright(0.5)} ,${scaleBright(1)})`;
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute('data-bright') ||
+    e.target.getAttribute('data-sat') ||
+    e.target.getAttribute('data-hue');
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = initialColors[index];
+
+  let color = chroma(bgColor)
+    .set('hsl.s', saturation.value)
+    .set('hsl.l', brightness.value)
+    .set('hsl.h', hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
+}
+
+function updateTextUI(index) {
+  const activeDiv = colorDivs[index];
+  const color = chroma(activeDiv.style.backgroundColor);
+  const textHex = activeDiv.querySelector('h2');
+  textHex.innerText = color.hex().toUpperCase();
+  checkTextColor(color, textHex);
+
+  const icons = activeDiv.querySelectorAll('.controls button');
+  icons.forEach((icon) => checkTextColor(color, icon));
 }
 
 randomColors();
